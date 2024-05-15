6352b722af6d7f75782f0ae0807dba0ec8cb03ae
harrycoder1991 <weskh1991@gmail.com>
2024-05-16 04:16:48 +0530

    updated

---
+++ billing-account-managment/src/main/java/com/cinfin/bam/config/RestTemplateConfig.java
@@ -0,0 +1,26 @@
+package com.cinfin.bam.config;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+  @Bean
+  public RestTemplate restTemplate() {
+    RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
+    restTemplate.setMessageConverters(Arrays.asList(new ByteArrayHttpMessageConverter(),
+        new StringHttpMessageConverter(StandardCharsets.UTF_8), new FormHttpMessageConverter(),
+        new MappingJackson2HttpMessageConverter(), new MappingJackson2XmlHttpMessageConverter()));
+    return restTemplate;
+  }
+}
